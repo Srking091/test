501e2b916dfb68be6a7c302aba20b79d4960ccc8
sqj <123123@qq.com>
2021-02-02 15:15:48 +0800

    测试提交

---
+++ src/main/java/entity/main.java
@@ -0,0 +1,11 @@
+package entity;
+
+/**
+ * @author mojiayi
+ * @date 2021-02-02 14:01
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

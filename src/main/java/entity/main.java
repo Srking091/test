3080614e1e4df80b84fd62d276781c72ad5eaf7f
sqj <123123@qq.com>
2021-02-02 15:24:09 +0800

    第二次测试

--- src/main/java/entity/main.java
+++ src/main/java/entity/main.java
@@ -7,5 +7,6 @@ package entity;
 public class main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("123");
     }
 }
---
+++ src/main/java/entity/stu.java
@@ -0,0 +1,11 @@
+package entity;
+
+/**
+ * @author mojiayi
+ * @date 2021-02-02 15:17
+ */
+public class stu {
+    public static void main(String[] args) {
+        System.out.println("456");
+    }
+}
